34b4fe57d26a77484ae6b8d7011ba8e494180287
patryk <gryzabawy@o2.pl>
2023-12-03 10:53:58 +0100

    defined moves range

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from pathlib import Path
 
 from pet import Pet
@@ -5,6 +7,8 @@ from pet import Pet
 coords_path: Path = Path.cwd() / 'coordinates'
 pets: list = ['cats_casual', 'cats_lazy', 'cats_pussies', 'mice']
 iterations: int = 5
+range_x: tuple[int, int] = (0, 100)
+range_y: tuple[int, int] = (0, 100)
 
 
 def create_objects(name: str, **kwargs) -> list:
@@ -32,41 +36,71 @@ def create_objects(name: str, **kwargs) -> list:
     return result
 
 
+def check_positions(pos: pd.DataFrame) -> pd.DataFrame:
+    """
+    Checking if actual positions of pets causes action.
+    :param pos: actual positions of pets as a row of pandas.DataFrame
+    :return: posirions of pets after actions as a row of pandas.DataFrame
+    """
+    pass
+
+
 def main():
 
     # create lists of Pet objects
     cats_casual: list = create_objects(
         name=pets[0],
         color='yellow',
-        max_move=10
+        max_move=10,
+        range_x=range_x,
+        range_y=range_y
     )
 
     cats_lazy: list = create_objects(
         name=pets[1],
         color='red',
         max_move=10,
-        counter=True
+        counter=True,
+        range_x=range_x,
+        range_y=range_y
     )
 
     cats_pussies: list = create_objects(
         name=pets[2],
         color='orange',
         max_move=5,
-        max_dist=100
+        max_dist=100,
+        range_x=range_x,
+        range_y=range_y
     )
 
     mice: list = create_objects(
         name=pets[3],
         color='blue',
         max_move=1,
+        range_x=range_x,
+        range_y=range_y
     )
 
     all_pets: list = cats_casual + cats_lazy + cats_pussies + mice
 
     # create pandas.DataFrame
+    cols: list[str] = [all_pets[i].name for i in range(len(all_pets))]
+    df: pd.DataFrame = pd.DataFrame(
+        data=np.array([[None]*len(all_pets)]*iterations),
+        columns=cols
+    )
 
+    # simulation algorythm
+    for i in range(iterations):
+        for single_pet in all_pets:
+            single_pet.move()
+            df.loc[i, single_pet.name] = f'{single_pet.curr_x};{single_pet.curr_y}'
 
-    # simulation algorithm
+        # new position checking algorythm
+
+    print(df)
+
     # for i in range(iterations):
     #     # moves
     #     for m in range(len(mice)):
--- pet.py
+++ pet.py
@@ -8,14 +8,19 @@ class Pet:
                  color: str,
                  start_x: int,
                  start_y: int,
+                 range_x: tuple[int, int],
+                 range_y: tuple[int, int],
                  max_move: int,
                  max_dist: int | None = None,
                  counter: bool = False
+
                  ) -> None:
         self.name: str = name
         self.color: str = color
         self.start_x: int = start_x
         self.start_y: int = start_y
+        self.range_x: tuple[int, int] = range_x
+        self.range_y: tuple[int, int] = range_y
         self.max_move: int = max_move
         self.max_dist: int = max_dist
         self.counter: bool = counter
@@ -27,10 +32,19 @@ class Pet:
         self.n: int | None = 0 if self.counter is True else None
 
     def move(self) -> None:
-        step_x: int = rd.randint(-self.max_move, self.max_move)
-        step_y: int = rd.randint(-self.max_move, self.max_move)
-        self.curr_x += step_x
-        self.curr_y += step_y
+        while True:
+            try:
+                step_x: int = rd.randint(-self.max_move, self.max_move)
+                step_y: int = rd.randint(-self.max_move, self.max_move)
+                assert self.range_x[0] < step_x < self.range_x[1]
+                assert self.range_y[0] < step_y < self.range_y[1]
+            except AssertionError:
+                continue
+            else:
+                self.curr_x += step_x
+                self.curr_y += step_y
+                break
+
         self.all_coords.append((self.curr_x, self.curr_y))
 
     def back_to_start(self) -> None:
